d17e8c780a1dc0ad7ab9dde9117002306b34c251
Kyrillus Mehanni <kyrillus.dev@gmail.com>
2022-07-11 19:50:40 +0200

    fixed team bug

--- components/team.tsx
+++ components/team.tsx
@@ -5,43 +5,43 @@ function Team() {
         <div id="team">
             <section className="h-screen grid place-items-center w-full min-h-screen min-h-[50em] bg-white">
                 <div className="flex-col text-8xl pl-[10vw] pr-[10vw] pt-12 pb-12">
-                    <h1 className="font-extrabold">Team</h1>
-                    <div className="container mx-auto px-4 mt-32">
+                    <h1 className="font-extrabold grid place-self-start">Team</h1>
+                    <div className=" mx-auto px-4 mt-32">
 
                         <div className="flex flex-wrap -m-7 mb-12">
                           <div className="w-full md:w-1/2 p-7">
                             <div className="flex flex-wrap -m-3.5">
                               <div className="w-auto p-3.5">
-                                <img className="w-32 h-32 rounded-full object-cover" src="/team/lisa_reiss.png" alt=""/>
+                                <img className="w-32 h-32 rounded-full object-cover" src="/team/lisa.png" alt=""/>
                               </div>
                               <div className="flex-1 p-3.5">
                                 <p className="mb-6 text-lg text-gray-700 italic">"Bereits in der Schule habe ich mich für die Entwicklung von neuen Produkten interessiert und ... Bei Smiling Food bin ich für die Produktentwicklung sowie das Marketing zuständig ..."</p>
-                                <h3 className="font-heading font-bold text-lg text-[#628474]">Lisa Reiss, <span className="font-light">Marketing & Sales</span></h3>
+                                <h3 className="font-heading font-bold text-lg text-[#628474]">Lisa, <span className="font-light">Marketing & Sales</span></h3>
                               </div>
                             </div>
                           </div>
                           <div className="w-full md:w-1/2 p-7">
-                            <div className="flex flex-wrap -m-3.5">
-                              <div className="p-3.5">
-                                <img className="w-32 h-32 rounded-full object-cover" src="/team/maximilian_geyer.png" alt=""/>
-                              </div>
-                              <div className="flex-1 p-3.5">
-                                <p className="mb-6 text-lg text-gray-700">Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Eleifend id vulpu tate amet nunc. Elit sagittis sollici tudin cursus aliquam quis dui. Ut curabitur morbi nunc sit leo tellus neque dolor. Amet sem egestas.</p>
-                                <h3 className="font-heading font-bold text-lg text-[#628474]">Maximilian Geyer, <span className="font-light">Operations & Business Development</span></h3>
-                              </div>
-                            </div>
-                          </div>
+                                                      <div className="flex flex-wrap -m-3.5">
+                                                        <div className="w-auto p-3.5">
+                                                          <img className="w-32 h-32 rounded-full object-cover" src="/team/lisa.png" alt=""/>
+                                                        </div>
+                                                        <div className="flex-1 p-3.5">
+                                                          <p className="mb-6 text-lg text-gray-700 italic">"Bereits in der Schule habe ich mich für die Entwicklung von neuen Produkten interessiert und ... Bei Smiling Food bin ich für die Produktentwicklung sowie das Marketing zuständig ..."</p>
+                                                          <h3 className="font-heading font-bold text-lg text-[#628474]">Lisa, <span className="font-light">Marketing & Sales</span></h3>
+                                                        </div>
+                                                      </div>
+                                                    </div>
                         </div>
                         <div className="flex flex-wrap -m-7">
                           <div className="w-full md:w-1/2 p-7">
                             <div className="flex flex-wrap -m-3.5">
                               <div className="flex-1 p-3.5 order-1">
                                 <p className="mb-6 text-lg text-gray-700">Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Eleifend id vulpu tate amet nunc. Elit sagittis sollici tudin cursus aliquam quis dui. Ut curabitur morbi nunc sit leo tellus neque dolor. Amet sem egestas.</p>
-                                <h3 className="font-heading font-bold text-lg text-[#628474]">Kyrillus Mehanni, <span className="font-light">Web Development</span></h3>
+                                <h3 className="font-heading font-bold text-lg text-[#628474]">Kyrillus, <span className="font-light">Web Development</span></h3>
 
                               </div>
                               <div className="w-auto p-3.5 md:order-1">
-                                <img className="w-32 h-32 rounded-full object-cover" src="/team/kyrillus_mehanni.png" alt=""/>
+                                <img className="w-32 h-32 rounded-full object-cover" src="/team/kyrillus.png" alt=""/>
                               </div>
                             </div>
                           </div>
@@ -49,10 +49,10 @@ function Team() {
                             <div className="flex flex-wrap -m-3.5">
                               <div className="flex-1 p-3.5 order-1">
                                 <p className="mb-6 text-lg text-gray-700">Lorem ipsum dolor sit amet, consec tetur adipis cing elit. Eleifend id vulpu tate amet nunc. Elit sagittis sollici tudin cursus aliquam quis dui. Ut curabitur morbi nunc sit leo tellus neque dolor. Amet sem egestas.</p>
-                                <h3 className="font-heading font-bold text-lg text-[#628474]">Sami Elyas, <span className="font-light">Product Development</span></h3>
+                                <h3 className="font-heading font-bold text-lg text-[#628474]">Sami, <span className="font-light">Product Development</span></h3>
                               </div>
                               <div className="w-auto p-3.5 md:order-1">
-                                <img className="w-32 h-32 rounded-full object-cover" src="/team/sami_elyas.png" alt=""/>
+                                <img className="w-32 h-32 rounded-full object-cover" src="/team/sami.png" alt=""/>
                               </div>
                             </div>
                           </div>
--- components/vision.tsx
+++ components/vision.tsx
@@ -5,12 +5,12 @@ function Vision() {
     return (
         <div id="vision">
             <section className="h-screen grid place-items-center min-h-[50em] w-full min-h-screen bg-[#f2f1ec] ">
-                <div className="flex-col pl-[10vw] pr-[10vw] space-y-2 md:space-y-12 w-full">
-                    <h1 className="text-8xl h-[20vh] text-[#3f4739] font-extrabold">
+                <div className="flex-col pl-[10vw] pr-[10vw]  w-full">
+                    <h1 className="text-8xl text-[#3f4739] font-extrabold">
                         Unsere Vision
                     </h1>
 
-                    <div className="flex flex-row h-1/2 w-full text-2xl xl:text-3xl 2xl:text-4xl pt-12 md:pt-4">
+                    <div className="flex flex-row w-full text-2xl xl:text-3xl 2xl:text-4xl pt-12 md:pt-4">
                         <h3 className="leading-relaxed h-[20vh]">
                             Wir möchten den Menschen helfen, sich gesünder zu ernähren, indem wir eine Marke aufbauen, die sich auf zuckerfreie Produkte konzentriert, bei denen unsere Kunden darauf vertrauen können, dass nur hochwertige Zutaten und gesunde Zuckerersatzstoffe verwendet werden.
 
