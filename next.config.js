55c57df8a2e6abfa5f55f052e14adf3490012c4c
Kyrillus Mehanni <kyrillus.dev@gmail.com>
2022-07-11 15:41:26 +0200

    seo changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+      const headers = [];
+      if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+        headers.push({
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+          source: '/:path*',
+        });
+      }
+      return headers;
+    },
 }
